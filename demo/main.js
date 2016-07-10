89a2501cc00e8502416d789b1d524851c486231b
yasemincidem <yasemincidem@gmail.com>
2016-07-10 22:02:31 +0300

    console regexp is corrected

--- demo/main.js
+++ demo/main.js
@@ -5,5 +5,4 @@
 function testFile(){
     console.log("test log"); test ; console.debug("test debug");
     console.error("test error"); console.debug("test debug");
-    debug ;console.info("test info");
 }
