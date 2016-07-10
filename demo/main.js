efceb3ad10794a1eab41c9c2598caeaef20f4429
yasemincidem <yasemincidem@gmail.com>
2016-07-10 22:00:19 +0300

    console regexp is corrected

--- demo/main.js
+++ demo/main.js
@@ -2,5 +2,8 @@
  * Created by yasem on 10.07.2016.
  */
 //Demo file created for test
-console.log("test log"); test ; console.debug("test debug") ;
-console.error("test error"); console.debug("test debug"); debug ;console.info("test info")
+function testFile(){
+    console.log("test log"); test ; console.debug("test debug");
+    console.error("test error"); console.debug("test debug");
+    debug ;console.info("test info");
+}
