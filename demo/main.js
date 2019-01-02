02b3511206e4154641f22cc15061537627ffab1e
FSC EHC <fscehc2@openmailbox.org>
2019-01-02 09:53:30 +0100

    RegExp was not working properly. Now only works if console.xxx ends with a ;

--- demo/main.js
+++ demo/main.js
@@ -3,9 +3,12 @@
  */
 //Demo file created for test
 function testFile(){
+        let a = "b";
         console.log("These");
         console.warn('console');
         console.error('statements');
         console.dir({ will: be, removed: "true" })
         console.log("except for this one!");/*NotClearConsole*/
+        console.log("this will fail (" + b + ")");
+        console.log("this will fail too (" + b + ")"); /*NotClearConsole*/
 }
